c8350e54fb546a7716eeed8040e96ad155ff6ac5
manas1820 <manasgupta1820@gmail.com>
2021-03-21 02:38:15 +0530

    ⚡ Hiding API

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import requests
 import json
+import config
 from googlesearch import search
 
 # function to get the link of the company using the name
 def get_emails(tag, n, language):
     urls = [url for url in search(tag, num_results=n, lang=language)][:n]
-    response = requests.get("https://api.hunter.io/v2/domain-search?domain="+urls[0]+"&api_key=48398ffff7e3b58d5148fe5c4c2be4646d671313")
+    response = requests.get("https://api.hunter.io/v2/domain-search?domain="+urls[0]+"&api_key="+config.api_secret)
     return response.json()
 
 
