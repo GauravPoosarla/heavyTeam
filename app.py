df3f723d1cc6b581088ad3e1259023c8a1003185
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-03-21 01:17:33 +0530

    git added home page navbar

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route("/")
+
+
+@app.route("/") 
 def home():
-    return render_template("home.html") 
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
