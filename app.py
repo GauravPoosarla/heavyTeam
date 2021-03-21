c8b1514578eb05078ace5430e16a4a58538731af
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-03-21 06:20:15 +0530

    added employee card

--- app.py
+++ app.py
@@ -4,18 +4,23 @@ from wtforms import StringField,  SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo
 from wtforms.validators import ValidationError
 
+
 class Companyform(FlaskForm):
 
     companyname = StringField('Company Name', validators=[DataRequired()])
-    country = SelectField('pick country:', choices=['United States', 'India', 'Singapore', 'Europe', 'Thailand', 'Padova'])
-    category = SelectField('Category', choices=[ 'Automotiv', 'Arts&crafts', 'Apparel&Fashion', 'Airlines', 'Accounting'])
+    country = SelectField('pick country:', choices=[
+                          'United States', 'India', 'Singapore', 'Europe', 'Thailand', 'Padova'])
+    category = SelectField('Category', choices=[
+                           'Automotiv', 'Arts&crafts', 'Apparel&Fashion', 'Airlines', 'Accounting'])
     submit = SubmitField('Enter')
+
 
 class employeeform(FlaskForm):
 
     empname = StringField('username', validators=[DataRequired()])
-    empdesign = StringField('username', validators=[DataRequired()])    
+    empdesign = StringField('username', validators=[DataRequired()])
     submit = SubmitField('Enter')
+
 
 app = Flask(__name__)
 
@@ -23,8 +28,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SECRET_KEY'] = 'any secret string'
 
 
-
-@app.route("/") 
+@app.route("/")
 def home():
     return render_template("home.html")
 
@@ -37,16 +41,18 @@ def companyinfo():
         session['country'] = form.country.data
         session['category'] = form.category.data
 
-    return render_template('companyinfo.html', form = form)
+    return render_template('companyinfo.html', form=form)
 
-@app.route("/employeeinfo", methods=['GET', 'POST'])   
+
+@app.route("/employeeinfo", methods=['GET', 'POST'])
 def employeeinfo():
     form = employeeform()
     if form.validate_on_submit():
         empname = form.empname.data
         empdesign = form.empdesign.data
 
-    return render_template('empinfo.html', form = form)
+    return render_template('employeeinfo.html', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
